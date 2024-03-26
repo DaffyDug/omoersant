dc20ff6f1f27de27a09e3edfc19b670f1b9cc3e0
Александр Александр <stripalol@inbox.ru>
2024-03-26 12:22:12 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication55/ConsoleApplication55.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+/*
+* идеальная ситуация, средняя, худшая
+* сложность поиска элемента contains
+*	О(1),			   O(N/2),	O(N),
+*
+* бинарный поиск (масив отсортирован)
+*	O(1),			  O(log N), O(log N)
+*/
+
+
+
+int main()
+{
+	int* arr = new int[10];
+	cout << *(arr + 5);
+}
